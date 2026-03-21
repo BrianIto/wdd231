a1ab1e2a8e5878b183671a271a98093545756294
Brian Moura <brianmoura@Mac-mini-de-Brian.local>
2026-03-21 14:02:40 -0400

    fix: added the grid/list toggle

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,5 +1,10 @@
+const lastModification = document.lastModified;
+const lastModifiedElement = document.getElementById("last-modified");
+
+lastModifiedElement.textContent = `Last Modified: ${lastModification}`;
+
 async function getDirectoriesFromFile() {
-	const response = await fetch("/scripts/members.json");
+	const response = await fetch("scripts/members.json");
 	const data = await response.json();
 	console.log(data);
 	return data;
@@ -7,12 +12,49 @@ async function getDirectoriesFromFile() {
 
 getDirectoriesFromFile().then((directories) => {
 	const directoryList = document.getElementById("directory-list");
-	directories.forEach((directory) => {
-		const listItem = document.createElement("li");
-		const link = document.createElement("a");
-		link.href = directory.url;
-		link.textContent = directory.name;
-		listItem.appendChild(link);
+	for (let directory of directories) {
+		const listItem = document.createElement("div");
+		const image = document.createElement("img");
+		const name = document.createElement("h2");
+		const address = document.createElement("p");
+		const description = document.createElement("p");
+		const phone = document.createElement("p");
+		const website = document.createElement("a");
+
+		image.src = directory.imageFile;
+		image.alt = `${directory.name} logo`;
+		name.textContent = directory.companyName;
+		address.textContent = `Address: ${directory.address}`;
+		description.textContent = directory.description;
+		phone.textContent = `Phone: ${directory.phone}`;
+		website.textContent = "Website";
+		website.href = directory.website;
+
+		listItem.appendChild(image);
+		listItem.appendChild(name);
+		listItem.appendChild(description);
+		listItem.appendChild(address);
+		listItem.appendChild(phone);
+		listItem.appendChild(website);
 		directoryList.appendChild(listItem);
-	});
+	}
+});
+
+const listButton = document.getElementById("list-view");
+const gridButton = document.getElementById("grid-view");
+
+// Initial grid-view
+gridButton.setAttribute("disabled", "true");
+listButton.removeAttribute("disabled");
+
+listButton.addEventListener("click", () => {
+	document.getElementById("directory-list").classList.add("list-view");
+	listButton.setAttribute("disabled", "true");
+	gridButton.removeAttribute("disabled");
+});
+
+gridButton.addEventListener("click", () => {
+	document.getElementById("directory-list").classList.remove("list-view");
+	gridButton.setAttribute("disabled", "true");
+	listButton.removeAttribute("disabled");
 });
--- chamber/scripts/hamb.js
+++ chamber/scripts/hamb.js
@@ -1,5 +1,6 @@
 let menuBtn = document.querySelector("#menu");
 let nav = document.querySelector("nav");
+
 menuBtn.addEventListener("click", () => {
 	nav.classList.toggle("show");
 });

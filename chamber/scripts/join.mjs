25e2ea255d38f8604cd2be5a1a6ee08b50aa83b8
Brian Moura <brianmoura@Mac-mini-de-Brian.local>
2026-03-27 21:29:53 -0400

    fix: lets go

--- chamber/scripts/join.mjs
+++ chamber/scripts/join.mjs
@@ -1,1 +1,11 @@
 document.getElementById("timestamp").value = Date.now();
+
+let modalButtons = ["free-btn", "gold-btn", "silver-btn", "bronze-btn"];
+let modalIds = ["free-modal", "gold-modal", "silver-modal", "bronze-modal"];
+
+modalButtons.forEach((buttonId, index) => {
+	document.getElementById(buttonId).addEventListener("click", () => {
+		let modalElement = document.getElementById(modalIds[index]);
+		modalElement.showModal();
+	});
+});
